18ce0001c48068ed0688ba71b26e95754ffc07f7
Danijel <susakovich@gmail.com>
2022-11-07 21:42:22 +0100

    Add onclick bgcolor change

--- js_exercises/simple_calculator/main.js
+++ js_exercises/simple_calculator/main.js
@@ -9,24 +9,29 @@ document.getElementById("divideTwo").onclick = divTwo;
 function addZero() {
   total = 0;
   document.querySelector("#result").innerText = total;
+  document.querySelector("body").style.backgroundColor = "white";
 }
 
 function addThree() {
   total += 3;
   document.querySelector("#result").innerText = total;
+  document.querySelector("body").style.backgroundColor = "violet";
 }
 
 function addNine() {
   total += 9;
   document.querySelector("#result").innerText = total;
+  document.querySelector("body").style.backgroundColor = "orange";
 }
 
 function subTwo() {
   total -= 2;
   document.querySelector("#result").innerText = total;
+  document.querySelector("body").style.backgroundColor = "darkgrey";
 }
 
 function divTwo() {
   total /= 2;
   document.querySelector("#result").innerText = total;
+  document.querySelector("body").style.backgroundColor = "burlywood";
 }
