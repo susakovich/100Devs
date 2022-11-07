8dbccab6cc9ad0ae763831860918da9582514cb0
Danijel <susakovich@gmail.com>
2022-11-07 13:47:23 +0100

    Add js

--- class12/class-weekend-boring/js/main.js
+++ class12/class-weekend-boring/js/main.js
@@ -1,10 +1,13 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector("#check").addEventListener("click", check);
 
 function check() {
-
-  const day = document.querySelector('#day').value
-
+  const day = document.querySelector("#day").value;
   //Conditionals go here
-
-
+  if (day === "Tuesday" || day === "Thursday") {
+    document.getElementById("placeToSee").innerText = "CLASS DAY";
+  } else if (day === "Saturday" || day === "Sunday") {
+    document.getElementById("placeToSee").innerText = "WEEEEEKAND";
+  } else {
+    document.getElementById("placeToSee").innerText = "BORIIIIINGGGGG";
+  }
 }
