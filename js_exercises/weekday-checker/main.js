bf11aaee47c8e4b17c7f102ae567307ec5366455
Danijel <susakovich@gmail.com>
2022-11-08 21:59:19 +0100

    Add toLowerCase on #day input

--- js_exercises/weekday-checker/main.js
+++ js_exercises/weekday-checker/main.js
@@ -2,11 +2,11 @@ document.getElementById("check").onclick = check;
 // document.querySelector("#check").addEventListener("click",check);
 
 function check() {
-  const day = document.querySelector("#day").value;
+  const day = document.querySelector("#day").value.toLowerCase();
 
-  if (day === "Tuesday" || day === "Thursday") {
+  if (day === "tuesday" || day === "thursday") {
     document.getElementById("placeToSee").innerText = "CLASSS DAAAAY";
-  } else if (day === "Saturday" || day === "Sunday") {
+  } else if (day === "saturday" || day === "sunday") {
     document.getElementById("placeToSee").innerText = "WEEKENDDD";
   } else {
     document.getElementById("placeToSee").innerText = "BORRRRINGGGGG";
