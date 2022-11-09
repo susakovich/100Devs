35417166f1a57e50e7c5b4856d78f21200cb0080
Danijel <susakovich@gmail.com>
2022-11-09 10:04:33 +0100

    Add function to convert cels to fahr

--- class13/temperature-converter/js/main.js
+++ class13/temperature-converter/js/main.js
@@ -1,1 +1,17 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+// 0 => 32
+
+// need the value in celsius
+
+// convert from cels to fahr
+
+// show it
+
+document.getElementById("check").addEventListener("click", convertCels);
+
+function convertCels() {
+  let temp = document.getElementById("cels").value;
+  temp = (temp * 9) / 5 + 32;
+  document.getElementById("placeToYell").innerText = temp;
+}
