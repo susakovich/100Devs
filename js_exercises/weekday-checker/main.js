b5de2560d9bfc20d6327fcf6f2306e65d22aec92
Danijel <susakovich@gmail.com>
2022-11-10 22:26:34 +0100

    Add toUpperCase on inputs

--- js_exercises/weekday-checker/main.js
+++ js_exercises/weekday-checker/main.js
@@ -1,14 +1,19 @@
-document.getElementById("check").onclick = check;
-// document.querySelector("#check").addEventListener("click",check);
+document.getElementById("check").addEventListener("click", checkDay);
 
-function check() {
-  const day = document.querySelector("#day").value.toLowerCase();
+function checkDay() {
+  let day = document.getElementById("day").value.toLowerCase();
 
   if (day === "tuesday" || day === "thursday") {
-    document.getElementById("placeToSee").innerText = "CLASSS DAAAAY";
+    document.getElementById("placeToSee").innerText = "CLASS DAY!";
+    document.querySelector("body").style.backgroundColor = "purple";
+    document.querySelector("h2").style.color = "white";
   } else if (day === "saturday" || day === "sunday") {
-    document.getElementById("placeToSee").innerText = "WEEKENDDD";
+    document.getElementById("placeToSee").innerText = "WEEKENDDD!";
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector("h2").style.color = "white";
   } else {
-    document.getElementById("placeToSee").innerText = "BORRRRINGGGGG";
+    document.getElementById("placeToSee").innerText = "BORRING!";
+    document.querySelector("body").style.backgroundColor = "brown";
+    document.querySelector("h2").style.color = "white";
   }
 }
