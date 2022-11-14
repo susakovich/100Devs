3adfd1249472f43e92266395aaeb52b3b12d95a3
Danijel <susakovich@gmail.com>
2022-11-14 19:20:14 +0100

    Finish tasks

--- class14/simple-functions/js/main.js
+++ class14/simple-functions/js/main.js
@@ -1,5 +1,16 @@
 //create a function that adds two numbers and alerts the sum
-
+function addTwoNumbers(num1, num2) {
+  alert(num1 + num2);
+}
+addTwoNumbers(5, 5);
 //create a function that multiplys three numbers and console logs the product
-
+function multiplysThreeNumbers(num1, num2, num3) {
+  console.log(num1 * num2 * num3);
+}
+multiplysThreeNumbers(5, 5, 5);
 //create a function that divides two numbers and returns the ???
+function divides(num1, num2) {
+  let sum = num1 / num2;
+  return sum;
+}
+console.log(divides(10, 2));
