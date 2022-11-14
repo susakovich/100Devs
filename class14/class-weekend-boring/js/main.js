918f4c424ed107b09b28dd6b5fdef54a68904167
Danijel <susakovich@gmail.com>
2022-11-14 19:40:09 +0100

    Finish homework

--- class14/class-weekend-boring/js/main.js
+++ class14/class-weekend-boring/js/main.js
@@ -1,17 +1,17 @@
 //Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
 
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector("#check").addEventListener("click", check);
 
 function check() {
+  const day = document.querySelector("#day").value.toLowerCase();
 
-  const day = document.querySelector('#day').value
-
-  if(day === "tuesday" || day === "thursday"){
-    console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
-    console.log("BORING")
+  if (day === "tuesday" || day === "thursday") {
+    document.getElementById("placeToSee").innerText = "Class Day";
+  } else if (day === "saturday" || day === "sunday") {
+    document.getElementById("placeToSee").innerText = "It's the Weekend";
+  } else if (day === "wednesday") {
+    document.getElementById("placeToSee").innerText = "It's Humpday";
+  } else {
+    document.getElementById("placeToSee").innerText = "Boring!";
   }
-
 }
