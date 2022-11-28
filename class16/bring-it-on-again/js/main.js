a0c9531067093d4f707370e857cabb7f8900e3e6
Danijel <susakovich@gmail.com>
2022-11-28 22:16:52 +0100

    Small change to loopNumber function

--- class16/bring-it-on-again/js/main.js
+++ class16/bring-it-on-again/js/main.js
@@ -45,9 +45,7 @@ dayOfWeek();
 //Create a function that takes in a number. Console log all values from 1 to that number or greater, but count by 3
 function loopNumber(num) {
   for (let i = 1; i <= num; i++) {
-    if (i === 4) {
-      break;
-    }
+    i += 2;
     console.log(i);
   }
 }
