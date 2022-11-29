fcb8e8b1021dc6283af59e259983bc202a27812c
Danijel <susakovich@gmail.com>
2022-11-29 19:15:26 +0100

    Small change to checkDay function

--- js_exercises/weekday-checker/main.js
+++ js_exercises/weekday-checker/main.js
@@ -1,19 +1,19 @@
-document.getElementById("check").addEventListener("click", checkDay);
+// document.getElementById("check").addEventListener("click", checkDay);
 
-function checkDay() {
-  let day = document.getElementById("day").value.toLowerCase();
+// function checkDay() {
+//   let day = document.getElementById("day").value.toLowerCase();
 
-  if (day === "tuesday" || day === "thursday") {
-    document.getElementById("placeToSee").innerText = "CLASS DAY!";
-    document.querySelector("body").style.backgroundColor = "purple";
-    document.querySelector("h2").style.color = "white";
-  } else if (day === "saturday" || day === "sunday") {
-    document.getElementById("placeToSee").innerText = "WEEKENDDD!";
-    document.querySelector("body").style.backgroundColor = "green";
-    document.querySelector("h2").style.color = "white";
-  } else {
-    document.getElementById("placeToSee").innerText = "BORRING!";
-    document.querySelector("body").style.backgroundColor = "brown";
-    document.querySelector("h2").style.color = "white";
-  }
-}
+//   if (day === "tuesday" || day === "thursday") {
+//     document.getElementById("placeToSee").innerText = "CLASS DAY!";
+//     document.querySelector("body").style.backgroundColor = "purple";
+//     document.querySelector("h2").style.color = "white";
+//   } else if (day === "saturday" || day === "sunday") {
+//     document.getElementById("placeToSee").innerText = "WEEKENDDD!";
+//     document.querySelector("body").style.backgroundColor = "green";
+//     document.querySelector("h2").style.color = "white";
+//   } else {
+//     document.getElementById("placeToSee").innerText = "BORRING!";
+//     document.querySelector("body").style.backgroundColor = "brown";
+//     document.querySelector("h2").style.color = "white";
+//   }
+// }
