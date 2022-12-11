09c8d57e892ebc415fd55c766e1246080b2f3daa
Danijel <susakovich@gmail.com>
2022-12-11 11:50:31 +0100

    Finish exercise

--- class18 - superReview/temperature-converter/js/main.js
+++ class18 - superReview/temperature-converter/js/main.js
@@ -1,1 +1,14 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+document.querySelector("h1").addEventListener("click", celsToFahr);
+
+function celsToFahr() {
+  // Take value from input
+  let enteredVal = Number(document.querySelector("input").value);
+  // Convert it to celsius to fahrenheit using formula
+  let temp = enteredVal * 1.8 + 32;
+  // Show value to the user
+  document.querySelector(
+    "p"
+  ).innerText = `Your celsius in fahrenheit is ${temp} `;
+}
