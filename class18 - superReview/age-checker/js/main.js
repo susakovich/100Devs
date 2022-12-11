92fa404e2de14256225c4e508bb3e1f8ca3c9aeb
Danijel <susakovich@gmail.com>
2022-12-11 10:56:11 +0100

    Finish exercise

--- class18 - superReview/age-checker/js/main.js
+++ class18 - superReview/age-checker/js/main.js
@@ -6,8 +6,29 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
-
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+document.querySelector("h1").addEventListener("click", ageResult);
+
+function ageResult() {
+  let age = Number(document.querySelector("input").value);
+
+  let p = document.querySelector("p");
+
+  if (age < 16) {
+    p.innerText = `You are ${age} years old, you can't drive.`;
+  } else if (age < 18) {
+    p.innerText = `You are ${age} years old, you can't hate outside the club, because you can't even get in.`;
+  } else if (age < 21) {
+    p.innerText = `You are ${age} years old, you can't drink.`;
+  } else if (age < 25) {
+    p.innerText = `You are ${age} years old, you can't rent cars affordably.`;
+  } else if (age < 30) {
+    p.innerText = `You are ${age} years old, you can't rent fancy cars affordably.`;
+  } else {
+    p.innerText = `You are ${age} years old, there is nothing left to look forward too.`;
+  }
+}
