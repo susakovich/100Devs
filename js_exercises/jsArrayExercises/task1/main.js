5166b719d8ac8adce0242f504aebfb747376eed3
Danijel <susakovich@gmail.com>
2022-12-17 23:15:46 +0100

    Add Array Exercises

---
+++ js_exercises/jsArrayExercises/task1/main.js
@@ -0,0 +1,29 @@
+// Translate border-left-width to borderLeftWidth
+// importance: 5
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+// That is: removes all dashes, each word after dash becomes uppercased.
+
+// Examples:
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+// P.S. Hint: use split to split the string into an array, transform it and join back.
+
+function camelize(str) {
+  return (
+    str
+      .split("-") // splits 'my-long-word' into array ['my', 'long', 'word']
+      // capitalizes first letters of all array items except the first one
+      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+      .map((word, index) =>
+        index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+      )
+      .join("")
+  ); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+}
+
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("webkit-transition"));
