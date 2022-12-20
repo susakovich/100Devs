248d423fee20633c8312f6ea5c62269442376b87
Danijel <susakovich@gmail.com>
2022-12-20 20:54:06 +0100

    Finish exercise

---
+++ js_exercises/jsArrayExercises/task4/main.js
@@ -0,0 +1,13 @@
+// Sort in decreasing order
+// importance: 4
+// let arr = [5, 2, 1, -10, 8];
+
+// // ... your code to sort it in decreasing order
+
+// alert( arr ); // 8, 5, 2, 1, -10
+
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b) => b - a);
+
+console.log(arr);
