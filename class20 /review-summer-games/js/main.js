2086962f906a4c4df9d4a05431654a3f94b6abca
Danijel <susakovich@gmail.com>
2022-12-22 12:30:07 +0100

    Finish exercise

--- class20 /review-summer-games/js/main.js
+++ class20 /review-summer-games/js/main.js
@@ -1,1 +1,15 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product.
+
+function multiplyNums(arr) {
+  let product = 1;
+  //   using forEach
+  //   arr.forEach((num) => (product *= num));
+
+  // using for loop
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  alert(product);
+}
+
+multiplyNums([10, 2, 3]);
