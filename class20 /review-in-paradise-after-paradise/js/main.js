539a32394ad62462d74f155a34e0aedf52c17c97
Danijel <susakovich@gmail.com>
2022-12-22 10:47:14 +0100

    Finish exercise

--- class20 /review-in-paradise-after-paradise/js/main.js
+++ class20 /review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,15 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function checkFirstLast(arr) {
+  if (arr[0] < arr[arr.length - 1]) {
+    alert("Hi");
+  } else if (arr[0] > arr[arr.length - 1]) {
+    alert("Bye");
+  } else {
+    alert("We close in an hour.");
+  }
+}
+
+checkFirstLast([123, 23, 45, 1, 2]);
+checkFirstLast([123, 23, 45, 1, 126]);
+checkFirstLast([123, 23, 45, 1, 123]);
