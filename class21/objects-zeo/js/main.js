1f671b46c7ce89c023621f85bd5cb58d12c63a8e
Danijel <susakovich@gmail.com>
2022-12-30 18:46:12 +0100

    Finish object tasks

--- class21/objects-zeo/js/main.js
+++ class21/objects-zeo/js/main.js
@@ -1,1 +1,18 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {};
+
+stopwatch.color = "black";
+stopwatch.brand = "citizen";
+stopwatch.shape = "round";
+stopwatch.size = "medium";
+
+stopwatch.start = function () {
+  console.log(`Starting!`);
+};
+
+stopwatch.stop = () => console.log(`Stoping!`);
+
+stopwatch.sayBrand = () =>
+  console.log(`Your watch brand is ${stopwatch.brand}.`);
+stopwatch.sayBrand();
