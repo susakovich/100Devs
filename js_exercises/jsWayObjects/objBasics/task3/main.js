59364a59cae3fcf6b83a91f5f5a3750519f48690
Danijel <susakovich@gmail.com>
2023-01-06 12:02:04 +0100

    Finish obj task3

---
+++ js_exercises/jsWayObjects/objBasics/task3/main.js
@@ -0,0 +1,26 @@
+// Sum object properties
+// importance: 5
+// We have an object storing salaries of our team:
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+// If salaries is empty, then the result must be 0.
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+let sum = 0;
+
+//* za objekte koristimo for in petlju
+for (let salary in salaries) {
+  sum += salaries[salary];
+}
+console.log(sum);
