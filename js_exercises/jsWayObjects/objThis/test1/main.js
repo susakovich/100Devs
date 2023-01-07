4691344f4540ece61018a8a752171f6e47ee75cb
Danijel <susakovich@gmail.com>
2023-01-07 13:49:37 +0100

    Finish task1

---
+++ js_exercises/jsWayObjects/objThis/test1/main.js
@@ -0,0 +1,32 @@
+// Create a calculator
+// importance: 5
+// Create an object calculator with three methods:
+
+// read() prompts for two values and saves them as object properties with names a and b respectively.
+// sum() returns the sum of saved values.
+// mul() multiplies saved values and returns the result.
+// let calculator = {
+// };
+
+// calculator.read();
+// alert( calculator.sum() );
+// alert( calculator.mul() );
+
+let calculator = {
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read() {
+    this.a = +prompt("a?", 0);
+    this.b = +prompt("b?", 0);
+  },
+};
+
+calculator.read();
+console.log(calculator.sum());
+console.log(calculator.mul());
