9377bf2280eb04c0b196e1c47594f123747c6dae
Danijel <susakovich@gmail.com>
2023-01-11 14:14:31 +0100

    Finish one more contstructor task

--- class23/objects-tony-hawk-pro-skater/js/main.js
+++ class23/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,26 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+const TonyHawkCharacter = function (chName, chStance, chSponsor, chMove) {
+  this.characterName = chName;
+  this.stance = chStance;
+  this.sponsor = chSponsor;
+  this.specialMove = chMove;
+
+  this.flip = function () {
+    console.log(`${this.characterName} just did a kickflip!`);
+  };
+
+  this.taunt = function () {
+    console.log(`Don't make me use my ${this.specialMove}!`);
+  };
+
+  this.grab = function () {
+    console.log(`Melon Grabbbbb!`);
+  };
+};
+
+let susak = new TonyHawkCharacter("Danijel", "Goofy", "Apple", "Ghetto Bird");
+console.log(susak);
+susak.flip();
+susak.grab();
+susak.taunt();
