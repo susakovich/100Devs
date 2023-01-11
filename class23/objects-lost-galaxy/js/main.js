0577c46874a0087b01e10515f257246733acaec4
Danijel <susakovich@gmail.com>
2023-01-11 13:51:42 +0100

    Add factory function, contstructor

--- class23/objects-lost-galaxy/js/main.js
+++ class23/objects-lost-galaxy/js/main.js
@@ -4,17 +4,15 @@ const dog = {};
 
 dog.eyeColor = "Blue";
 dog.name = "Loki";
-dog.bread = "Golden Retriver";
+dog.breed = "Golden Retriver";
 dog.hairColor = "Golden";
 
 dog.bark = function () {
   console.log(`Grr, grrr, roar!`);
 };
-
 dog.givePaw = function () {
   console.log(`${dog.name} has given you his paw!`);
 };
-
 dog.sitDown = function () {
   console.log(`${dog.name} has sit down!`);
 };
@@ -23,3 +21,26 @@ console.log(dog);
 dog.sitDown();
 dog.givePaw();
 dog.bark();
+
+// Make a function factory aka contructor
+const MakeDog = function (eyeColor, name, breed, hairColor) {
+  this.eyeColor = eyeColor;
+  this.name = name;
+  this.breed = breed;
+  this.hairColor = hairColor;
+
+  this.bark = function () {
+    console.log(`Grr, grrr, roar!`);
+  };
+  this.givePaw = function () {
+    console.log(`${this.name} has given you his paw!`);
+  };
+  this.sitDown = function () {
+    console.log(`${this.name} has sit down!`);
+  };
+};
+
+let rex = new MakeDog("Brown", "Rex", "German Shepard", "Brown");
+console.log(rex);
+rex.givePaw();
+rex.sitDown();
