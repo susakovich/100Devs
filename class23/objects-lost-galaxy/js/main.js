36a3d09270eaee2dedade6684e125b34a3ff52a4
Danijel <susakovich@gmail.com>
2023-01-11 13:45:46 +0100

    Finish object tasks

--- class23/objects-lost-galaxy/js/main.js
+++ class23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,25 @@
 //Create a dog object that has four properties and three methods
+
+const dog = {};
+
+dog.eyeColor = "Blue";
+dog.name = "Loki";
+dog.bread = "Golden Retriver";
+dog.hairColor = "Golden";
+
+dog.bark = function () {
+  console.log(`Grr, grrr, roar!`);
+};
+
+dog.givePaw = function () {
+  console.log(`${dog.name} has given you his paw!`);
+};
+
+dog.sitDown = function () {
+  console.log(`${dog.name} has sit down!`);
+};
+
+console.log(dog);
+dog.sitDown();
+dog.givePaw();
+dog.bark();
