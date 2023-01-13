7a6eca170e844815a88d62658bffd1951d3ca927
Danijel <susakovich@gmail.com>
2023-01-13 23:13:49 +0100

    Finish object literal notation task

--- class26/objects-lost-galaxy/js/main.js
+++ class26/objects-lost-galaxy/js/main.js
@@ -1,1 +1,25 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {};
+
+pizza.size = "large";
+pizza.topings = [
+  "turkey ham",
+  "onion",
+  "corn",
+  "chilli paprika",
+  "gauda",
+  "garlic",
+];
+pizza.crust = "stuffed";
+pizza.sauce = "hot ketchup";
+
+pizza.estimatedDeliveryTime = function () {
+  console.log(`Calculating....`);
+};
+pizza.burnMouth = function () {
+  console.log(`Are you satisfied now? See how hot I can be?!`);
+};
+pizza.frisbee = function () {
+  console.log(`Come on, I'm here to be eaten, not to play around with me....`);
+};
