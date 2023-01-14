09544d16eaf39ceb44c5939fe057fcda62d0ebd1
Danijel <susakovich@gmail.com>
2023-01-14 23:06:36 +0100

    Refactor from constructor to class obj

--- class26/objects-constructor-fun/js/main.js
+++ class26/objects-constructor-fun/js/main.js
@@ -1,22 +1,24 @@
 //Create a constructor with 4 properties and 3 methods
 
-function MakePizza(size, toppings, crust, sauce) {
-  this.size = size;
-  this.toppings = toppings;
-  this.crust = crust;
-  this.sauce = sauce;
+class MakePizza {
+  constructor(size, toppings, crust, sauce) {
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.sauce = sauce;
 
-  this.estimatedDeliveryTime = function () {
-    console.log(`Calculating....`);
-  };
-  this.burnMouth = function () {
-    console.log(`Are you satisfied now? See how hot I can be?!`);
-  };
-  this.frisbee = function () {
-    console.log(
-      `Come on, I'm here to be eaten, not to play around with me....`
-    );
-  };
+    this.estimatedDeliveryTime = function () {
+      console.log(`Calculating....`);
+    };
+    this.burnMouth = function () {
+      console.log(`Are you satisfied now? See how hot I can be?!`);
+    };
+    this.frisbee = function () {
+      console.log(
+        `Come on, I'm here to be eaten, not to play around with me....`
+      );
+    };
+  }
 }
 
 let capricoza = new MakePizza(
