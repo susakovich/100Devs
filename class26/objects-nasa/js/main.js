5a6d7c13039b2a48c47699fb61e2250663d6ce13
Danijel <susakovich@gmail.com>
2023-01-15 13:14:13 +0100

    Finish NASA Api project

--- class26/objects-nasa/js/main.js
+++ class26/objects-nasa/js/main.js
@@ -1,1 +1,13 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+
+fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    document.querySelector("h2").innerText = data.title;
+    document.querySelector("img").src = data.hdurl;
+    document.querySelector("h3").innerText = data.explanation;
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
