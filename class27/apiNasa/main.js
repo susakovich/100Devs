f1628524c31223ee664a529e544d1d87c23269d9
Danijel <susakovich@gmail.com>
2023-01-20 00:04:25 +0100

    Finish nasa api

---
+++ class27/apiNasa/main.js
@@ -0,0 +1,20 @@
+//Example fetch using pokemonapi.co
+document.querySelector("button").addEventListener("click", getFetch);
+
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  console.log(choice);
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=9mXswgsd6t6FDxKWG6CfbFLoHVxxcmPdAfy6bs0t&date=${choice}`;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      document.querySelector("img").src = data.hdurl;
+      document.querySelector("h3").innerText = data.explanation;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
