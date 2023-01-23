84a03b4ac054ae1dd97a4ed34f95088f9e14379f
Danijel <susakovich@gmail.com>
2023-01-23 22:45:16 +0100

    Finish dnd api

--- class28/dnd/js/main.js
+++ class28/dnd/js/main.js
@@ -1,17 +1,24 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      data.subclasses.forEach((obj) => {
+        // create an li
+        const li = document.createElement("li");
+        // add text to li
+        li.textContent = obj.name;
+        // apend the li to the ul
+        document.querySelector("ul").appendChild(li);
       });
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
-
