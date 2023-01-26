dddfe9a9f27ccba89315352052e671e4641fb6f0
Danijel <susakovich@gmail.com>
2023-01-26 22:11:59 +0100

    Finish books api

--- class28/bookAPI/js/main.js
+++ class28/bookAPI/js/main.js
@@ -1,27 +1,25 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  console.log(choice)
-  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
+document.querySelector("h2").innerText = localStorage.getItem("books");
 
+function getFetch() {
+  let choice = document.querySelector("input").value;
+  const url = `https://openlibrary.org/isbn/${choice}.json`;
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        if(data.media_type === "image"){
-          document.querySelector('img').src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-        }else{
-          alert('Media Not Supported - Contact NASA IMMEDIATLY')
-        }
-       
-        document.querySelector('h3').innerText = data.explanation
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      if (!localStorage.getItem("books")) {
+        localStorage.setItem("books", data.title);
+      } else {
+        // put title into localStorage
+        let books = localStorage.getItem("books") + "; " + data.title;
+        localStorage.setItem("books", books);
+        // put localStorage books key into DOM
+      }
+      document.querySelector("h2").innerText = localStorage.getItem("books");
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
-
