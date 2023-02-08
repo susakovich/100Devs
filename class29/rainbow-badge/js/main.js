683edf2dd6728e31dc7a69d76ba6e433e3a66582
Danijel <susakovich@gmail.com>
2023-02-08 23:31:02 +0100

    Add poke img and type to the dom

--- class29/rainbow-badge/js/main.js
+++ class29/rainbow-badge/js/main.js
@@ -1,45 +1,41 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const poke1 = document.querySelector('#poke1').value
-  const poke2 = document.querySelector('#poke2').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+poke1
-  const url2 = 'https://pokeapi.co/api/v2/pokemon/'+poke2
-  let pokeStore = []
-  let pokeImg = []
+function getFetch() {
+  const poke1 = document.querySelector("#poke1").value;
+  const poke2 = document.querySelector("#poke2").value;
+  const url = `https://pokeapi.co/api/v2/pokemon/${poke1}`;
+  const url2 = `https://pokeapi.co/api/v2/pokemon/${poke2}`;
+  let pokeStore = [];
+  let pokeImg = [];
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      pokeStore.push(data.types[0].type.name);
+      pokeImg.push(data.sprites.front_shiny);
+      document.querySelector("#pokeImg1").src = data.sprites.front_shiny;
+      document.querySelector(
+        "#poke1Type"
+      ).innerText = `Pokemon type: ${data.types[0].type.name}`;
 
-        pokeStore.push(data.types[0].type.name)
-        pokeImg.push(data.sprites.front_shiny)
-        
-        fetch(url2)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-
-          pokeStore.push(data.types[0].type.name)
-          pokeImg.push(data.sprites.front_shiny)
-      
-          if((pokeStore[0] === "grass" && pokeStore[1] === 'water')){
-            document.querySelector('#pokeImg1').src = pokeImg[0]
-            document.querySelector('#pokeImg2').src = pokeImg[1]
-            document.querySelector('h2').innerText = " 2x > "
-          }
+      fetch(url2)
+        .then((res) => res.json()) // parse response as JSON
+        .then((data) => {
+          console.log(data);
+          pokeStore.push(data.types[0].type.name);
+          pokeImg.push(data.sprites.front_shiny);
+          document.querySelector("#pokeImg2").src = data.sprites.front_shiny;
+          document.querySelector(
+            "#poke2Type"
+          ).innerText = `Pokemon type: ${data.types[0].type.name}`;
         })
-        .catch(err => {
-            console.log(`error ${err}`)
+        .catch((err) => {
+          console.log(`error ${err}`);
         });
-
-
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-
-
-
-      
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
