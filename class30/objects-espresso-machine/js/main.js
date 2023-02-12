39dbde0b31ba6662f8b148f033ae776095fae88a
Danijel <susakovich@gmail.com>
2023-02-12 09:49:16 +0100

    Finish object task

--- class30/objects-espresso-machine/js/main.js
+++ class30/objects-espresso-machine/js/main.js
@@ -1,1 +1,31 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class CoffeeMachine {
+  constructor(color, make, model, price) {
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.price = price;
+
+    this.turnOn = function () {
+      console.log(`Good day. I am here for you.`);
+    };
+    this.turnOf = function () {
+      console.log(`Bye. I'm out!`);
+    };
+    this.brew = function () {
+      console.log(`Please wait, your coffee is brewing.`);
+    };
+  }
+}
+
+let deLonghi = new CoffeeMachine(
+  "Black",
+  "DeLonghi",
+  "Magnifica Evo One",
+  1100
+);
+console.log(machiatto);
+macchiato.turnOn();
+macchiato.turnOf();
+macchiato.brew();
