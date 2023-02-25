3875a281300a6ef0ae982db1bf0910279ad3ab68
Danijel <susakovich@gmail.com>
2023-02-25 21:58:23 +0100

    Finish pseudo code practice

--- superJS-Review/temperature-converter/js/main.js
+++ superJS-Review/temperature-converter/js/main.js
@@ -1,1 +1,18 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+// add click on h1
+document.querySelector("h1").addEventListener("click", celsToFahr);
+
+// celsius to fahrenheit
+function celsToFahr() {
+  // save cels temp
+  cels = document.querySelector("input").value;
+  // need the value in celsius
+  let temp = Number(document.querySelector("input").value);
+  // convert it to fahrenheit
+  temp = temp * 1.8 + 32;
+  // show it in the dom
+  document.querySelector(
+    "h2"
+  ).innerText = `${cels} celsius is ${temp} fahrenheit degrees `;
+}
