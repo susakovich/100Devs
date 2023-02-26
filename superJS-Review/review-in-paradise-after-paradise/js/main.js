781e8455b632efe23204e86ccee3b656a5bb719d
Danijel <susakovich@gmail.com>
2023-02-26 10:34:19 +0100

    Finish Array exercise

--- superJS-Review/review-in-paradise-after-paradise/js/main.js
+++ superJS-Review/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,13 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function lastFirstNum(arr) {
+  if (arr[0] < arr[arr.length - 1]) {
+    console.log(`Hi`);
+  } else if (arr[0] > arr[arr.length - 1]) {
+    console.log(`Bye`);
+  } else {
+    console.log(`We close in an hour.`);
+  }
+}
+lastFirstNum([1, 3, 4, 5, 10]);
+lastFirstNum([10, 3, 4, 5, 10]);
+lastFirstNum([11, 3, 4, 5, 10]);
