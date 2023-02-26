47ae3c688564aa011b1abce1e1e63a4cc51dc041
Danijel <susakovich@gmail.com>
2023-02-26 13:36:16 +0100

    Finish object task

--- superJS-Review/objects-listen-to-your-heart/js/main.js
+++ superJS-Review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,47 @@
 //Create a stopwatch object that has four properties and three methods
+
+// object literal
+let stopwatch = {};
+stopwatch.color = "black";
+stopwatch.shape = "round";
+stopwatch.brand = "omega";
+stopwatch.smell = "new";
+
+stopwatch.start = function () {
+  console.log(`RUNNNNN BRUH`);
+};
+stopwatch.stop = function () {
+  console.log(`STOP RUNNING BRUH`);
+};
+stopwatch.alarm = function () {
+  console.log(`Your alarm is set!`);
+};
+
+// class constructor
+class Stopwatch {
+  constructor(brand, model, color, shape) {
+    this.brand = brand;
+    this.model = model;
+    this.color = color;
+    this.shape = shape;
+
+    this.beep = function () {
+      console.log(`Beep, beep, beep.`);
+    };
+
+    this.startTimer = function () {
+      console.log(`Your timer just started.`);
+    };
+
+    this.stopTimer = function () {
+      console.log(`Your tmer just stopped.`);
+    };
+  }
+}
+
+let citizen = new Stopwatch(
+  "Citizen",
+  "World Chronograph A-T",
+  "Silver",
+  "Round"
+);
