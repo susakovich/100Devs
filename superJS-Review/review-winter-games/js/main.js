2a565d6ce8c75b72f6be91fbc69af60fab73933f
Danijel <susakovich@gmail.com>
2023-02-26 12:23:35 +0100

    Finish array task

--- superJS-Review/review-winter-games/js/main.js
+++ superJS-Review/review-winter-games/js/main.js
@@ -1,1 +1,17 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array . Bonus: (do not use map or filter)
+
+function evenNumsFinder(arr) {
+  let onlyEvens = [];
+  arr.forEach((num) => {
+    if (num % 2 === 0) {
+      onlyEvens.push(num);
+    }
+  });
+  return onlyEvens;
+}
+
+console.log(evenNumsFinder([23, 2, 4, 55, 120]));
+
+// using filter
+const evens = (arr) => arr.filter((num) => num % 2 === 0);
+console.log(evens([23, 4, 12, 113, 90, 77]));
