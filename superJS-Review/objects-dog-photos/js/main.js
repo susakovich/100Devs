2782509e7b2ddd03afa18a25a2964c39d1f407f7
Danijel <susakovich@gmail.com>
2023-02-26 14:54:04 +0100

    Finish Dog API

--- superJS-Review/objects-dog-photos/js/main.js
+++ superJS-Review/objects-dog-photos/js/main.js
@@ -1,1 +1,17 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+// on button click, generate picture
+document.querySelector("button").addEventListener("click", getPhoto);
+
+function getPhoto() {
+  const url = "https://dog.ceo/api/breeds/image/random";
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector("img").src = data.message;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
